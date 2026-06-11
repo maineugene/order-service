1d713557b7e8e95045ad4990e342a5390d2c2999
evgeniy <eugenio7002@gmail.com>
2026-06-11 16:32:15 +0300

    repository layer for Order

---
+++ src/main/java/com/innowise/orderservice/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.innowise.orderservice.repository;
+
+import com.innowise.orderservice.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
+    List<Order> findByUserId(Long userId);
+}
